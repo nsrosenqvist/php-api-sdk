2a2db71f4969e659d8712ac5e594ae1ec5b91364
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2022-12-10 17:33:14 +0100

    Fix setup script

--- dev/setup.php
+++ dev/setup.php
@@ -5,14 +5,13 @@ if (php_sapi_name() !== 'cli') {
     exit(1);
 }
 
-# Set root as working directory
-chdir(dirname(__DIR__));
+$root = dirname(__DIR__);
 
 # Add git hooks
 foreach (glob(__DIR__ . '/hooks/*') as $path) {
     $name = basename($path);
-    $target = "dev/hooks/{$name}";
-    $link = ".git/hooks/$name";
+    $target = realpath("dev/hooks/$name");
+    $link = $root . "/.git/hooks/$name";
 
     if (! is_link($link) && ! @file_exists($link)) {
         echo "Updating hooks: linking $name" . PHP_EOL;
